12eb44e2a7686e59532f7c2c42972fe210c5cd0e
LeewayTang <1009651761@qq.com>
2021-06-01 23:22:19 +0800

    实现了前两个功能

--- app/src/main/java/com/example/jboom/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/jboom/ui/gallery/GalleryFragment.java
@@ -1,9 +1,13 @@
 package com.example.jboom.ui.gallery;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,28 +18,71 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.jboom.R;
 import com.example.jboom.databinding.FragmentGalleryBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GalleryFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
     private FragmentGalleryBinding binding;
+    int c = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        return root;
+    }
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    @Override
+    public void onActivityCreated(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        TextView counter = (TextView)getActivity().findViewById(R.id.counter);
+        TextView timeCounter = (TextView)getActivity().findViewById(R.id.time_counter);
+        FloatingActionButton fab1 = (FloatingActionButton)getActivity().findViewById(R.id.fab1);
+        Button button = (Button)getActivity().findViewById(R.id.button);
+        button.setEnabled(false);
+        button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                counter.setText(Integer.parseInt((String) counter.getText())+1+"");
             }
         });
-        return root;
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter.setText("0");
+                timeCounter.setText("10s");
+                CountDownTimer countDownTimer0 = new CountDownTimer(4*1000, 1000) {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        button.setText(millisUntilFinished /1000+"");
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        button.setText("戳");
+                        CountDownTimer countDownTimer = new CountDownTimer(10 * 1000, 1000){
+                            @SuppressLint("SetTextI18n")
+                            @Override
+                            public void onTick(long millisUntilFinished) {
+                                timeCounter.setText(millisUntilFinished / 1000 + "s");
+                                button.setEnabled(true);
+                            }
+
+                            @Override
+                            public void onFinish() {
+                                timeCounter.setText("时间到");
+                                button.setEnabled(false);
+                            }
+                        };
+                        countDownTimer.start();
+                    }
+                };
+                countDownTimer0.start();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/jboom/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/jboom/ui/home/HomeFragment.java
@@ -1,10 +1,15 @@
 package com.example.jboom.ui.home;
 
+import android.app.Service;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,35 +17,60 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.jboom.Chronometer;
 import com.example.jboom.R;
 import com.example.jboom.databinding.FragmentHomeBinding;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.zip.Inflater;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    long stopTime;
+    boolean flag = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+//        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return root;
+    }
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    @Override
+    public void onActivityCreated(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Vibrator vibrator=(Vibrator)getActivity().getSystemService(Service.VIBRATOR_SERVICE);
+        Chronometer chronometer = getActivity().findViewById(R.id.chronometer2);
+        FloatingActionButton restart = (FloatingActionButton) getActivity().findViewById(R.id.fab);
+        Button stop = (Button) getActivity().findViewById(R.id.stop_chronometer);
+        restart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+//                Toast.makeText(getActivity(), "hey", Toast.LENGTH_LONG).show();
+                chronometer.stop();
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                stopTime = 0;
+                flag = false;
+                chronometer.start();
             }
         });
-        return root;
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.stop();
+                if(chronometer.getText().equals("10:00")){
+                    long[] patter = {1000, 1000, 2000, 50};
+                    vibrator.vibrate(patter, 0);
+                }
+            }
+        });
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+//        binding = null;
     }
 }
